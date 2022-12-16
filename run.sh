6718236db96f13df3c58524b659c6d696893b8ce
wrov <wrov@users.noreply.github.com>
2022-12-16 19:29:26 +0100

    Prevent docker termination during large downloads

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 while true; do
     echo "Starting service..."
-    gunicorn -b 0.0.0.0:8080 api:api
+    gunicorn --worker-class gthread --threads 1 -b 0.0.0.0:8080 api:api worker_class = 'gthread'
 done
